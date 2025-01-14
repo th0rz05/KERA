13ebf46770149f3491d9ff1a1b97fc670239a17b
Tiago Barbosa <tiago.filipe.barbosa@gmail.com>
2025-01-14 21:46:41 +0100

    Restore lecturers node in preloaded peers configuration

--- src/constants.py
+++ src/constants.py
@@ -30,6 +30,5 @@ BANNED_HOSTS = [
 ]
 
 PRELOADED_PEERS = {
-    #Peer("128.130.122.101", 18018), # lecturers node
-    Peer("127.0.0.1", 18019), # test node 1
+    Peer("128.130.122.101", 18018), # lecturers node
 }
